28e78b7cfff55d36da7193eaaa4b6530dd3d2188
Guillermo Santos <guillermo.santosba@gmail.com>
2023-02-15 16:09:37 -0600

    Added basic structure

---
+++ MoodLog/Screens/MOLogsVC.swift
@@ -0,0 +1,18 @@
+//
+//  MOLogsVC.swift
+//  MoodLog
+//
+//  Created by Guillermo Santos Barrios on 2/15/23.
+//
+
+import UIKit
+
+class MOLogsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        // Do any additional setup after loading the view.
+    }
+
+}
