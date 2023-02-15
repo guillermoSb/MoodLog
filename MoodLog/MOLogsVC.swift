d937ca8c1ade3e61f26793af7bd1c46d9805772c
Guillermo Santos <guillermo.santosba@gmail.com>
2023-02-15 16:07:37 -0600

    Initial Commit

---
+++ MoodLog/MOLogsVC.swift
@@ -0,0 +1,29 @@
+//
+//  MOLogsVC.swift
+//  MoodLog
+//
+//  Created by Guillermo Santos Barrios on 2/15/23.
+//
+
+import UIKit
+
+class MOLogsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MoodLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodLog
+//
+//  Created by Guillermo Santos Barrios on 2/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
