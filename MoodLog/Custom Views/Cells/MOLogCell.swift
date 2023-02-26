dedbf0f78ad1278b29f7cf61390e84f88c4e6e8e
Guillermo Santos <guillermo.santosba@gmail.com>
2023-02-25 19:10:32 -0600

    Joined log activity and mood in just one label

--- MoodLog/Custom Views/Cells/MOLogCell.swift
+++ MoodLog/Custom Views/Cells/MOLogCell.swift
@@ -12,8 +12,8 @@ class MOLogCell: UITableViewCell {
     static let reuseID = "LogCell"
     
     let logDateLabel = MOBodyLabel(with: .caption1)
-    let logMoodLabel = MOBodyLabel(with: .body)
-    let logActivityLabel = MOBodyLabel(with: .body)
+    let logInfoCell = MOBodyLabel(with: .body)
+    
     
     
     
@@ -30,29 +30,25 @@ class MOLogCell: UITableViewCell {
     private func configure() {
         let padding: CGFloat = 12.0
         contentView.addSubview(logDateLabel)
-        contentView.addSubview(logMoodLabel)
-        contentView.addSubview(logActivityLabel)
+        contentView.addSubview(logInfoCell)
+ 
         NSLayoutConstraint.activate([
             logDateLabel.topAnchor.constraint(equalTo: topAnchor, constant: padding),
             logDateLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
             logDateLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             logDateLabel.heightAnchor.constraint(equalToConstant: 22),
             
-            logMoodLabel.topAnchor.constraint(equalTo: logDateLabel.bottomAnchor, constant: padding),
-            logMoodLabel.leadingAnchor.constraint(equalTo: logDateLabel.leadingAnchor),
-            logMoodLabel.heightAnchor.constraint(equalToConstant: 22),
-
-            logActivityLabel.topAnchor.constraint(equalTo: logMoodLabel.topAnchor),
-            logActivityLabel.leadingAnchor.constraint(equalTo: logMoodLabel.trailingAnchor, constant: padding),
-            logActivityLabel.heightAnchor.constraint(equalToConstant: 22)
+            logInfoCell.topAnchor.constraint(equalTo: logDateLabel.bottomAnchor, constant: padding),
+            logInfoCell.leadingAnchor.constraint(equalTo: logDateLabel.leadingAnchor),
+            logInfoCell.heightAnchor.constraint(equalToConstant: 22),
         ])
         
     }
     
     func set(with log: Log) {
         logDateLabel.text = log.date.toLocaleString()
-        logMoodLabel.text = log.moodLevel.rawValue
-        logActivityLabel.text = log.activity.rawValue
+        logInfoCell.text = "\(log.moodLevel.rawValue) - \(log.activity.rawValue)"
+   
         
     }
     
